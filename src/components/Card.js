48ecffd16b28ca64654c4c9abca6785d28c72598
Anis Ahmad <anisahmad1770@gmail.com>
2023-08-23 23:56:05 +0500

    commit1

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div style={{height:"300px",width:"250px",border:"1px solid lightgray",display:"inline-block",padding:"10px",margin:"10px"}}>
+      <h3>Title</h3>
+      <p>This is Discription</p>
+    </div>
+  )
+}
+
+export default Card
