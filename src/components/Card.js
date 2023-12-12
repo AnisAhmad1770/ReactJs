d9e3d9d273e9d8d0f84fde2529305342289fd819
Anis Ahmad <83420587+AnisAhmad1770@users.noreply.github.com>
2023-12-12 12:17:02 +0500

    just commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ const Card = () => {
     <div style={{height:"300px",width:"250px",border:"1px solid lightgray",display:"inline-block",padding:"10px",margin:"10px"}}>
       <h3>Title</h3>
       <p>This is Discription</p>
+      
     </div>
   )
 }
